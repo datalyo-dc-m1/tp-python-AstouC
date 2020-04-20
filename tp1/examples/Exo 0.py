812001a4dad96bed6c50e766c56c8ba1429ac66c
Astou <astoucisse1996@gmail.com>
2020-04-20 14:15:32 +0200

    hey

---
+++ tp1/examples/Exo 0.py
@@ -0,0 +1,18 @@
+#exo0
+a = 5.3
+b = 9.70
+c = a+b
+print(c)
+c += a # qui veut dire C = c+a
+print (c)
+d = c
+print(d)
+d-=1 # d= d-1#
+print (d)
+# à la fin du programme c = 20,3
+
+""" maintenant j'affiche le type de variable de C 
+"""
+c_type = type(c)  # on récupère le type de c
+print(c_type)     # on affiche le type  et c'est un nombre décimale
+#exo1#
---
+++ tp2/imc.py
@@ -0,0 +1,24 @@
+def imc(poids=87,taille=167): #déclaration de la fonction imc#
+    '''
+
+    :param poids: le poids de la personne en kg
+    :param taille: la taille de la personne en cm
+    :return: l'imc de la personne
+    '''
+    imc=poids/((taille*taille)/100)
+    imc=round(imc * 100, 1)
+    if imc <= 18.5 : # Pour une condition, j'utilise "if" une fois et lorsque j'ai 3 conditions ou plus, on utilise Elif à la place de "if" à la deuxième ligne#
+        print ("vous êtes en maigreur")
+    elif imc <= 24.9 :
+        print ("vous avez un poids normal")
+    elif imc <= 29.9 :
+        print ("vous êtes en surpoids")
+    elif imc <= 40 :
+        print ("vous êtes en obésité")
+    else: #aprés chaque deux points, je vais obligatoirement à la ligne
+        print ("Vous êtes en obésité massive")
+    return imc
+
+
+mon_imc = imc()
+print (mon_imc)
