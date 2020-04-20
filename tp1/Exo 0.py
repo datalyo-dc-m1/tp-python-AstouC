3e2c99c39c351c47350ac6193c78446ed200d2b8
Astou <astoucisse1996@gmail.com>
2020-04-20 14:59:47 +0200

    Guest  number

--- tp1/Exo 0.py
+++ tp1/Exo 0.py
@@ -10,12 +10,14 @@ print(d)
 d-=1 # d= d-1#
 print (d)
 # à la fin du programme c = 20,3
-
+ # CORRECTION
 """ maintenant j'affiche le type de variable de C 
 """
 c_type = type(c)  # on récupère le type de c
 print(c_type)     # on affiche le type  et c'est un nombre décimale c'est à dire Float
 
 #A l'aide la fonction int(), créer une nouvelle variable c_int qui contient la valeur de la variable c
-c_int = int(c)      # on crée un entier qui contient la valeur entière de c
+c_int = int(c)      # int permet d'avoir un nombre entier à partir d'un nombre décimal 
+print("La valeur entière de c est : ",c_int)
+
 
